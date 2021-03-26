cc777feb0d739015d3102ef7139dd64f6356ae2b
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2021-03-26 18:53:01 +0600

    Update Orbit.cpp

--- Leet/Orbit.cpp
+++ Leet/Orbit.cpp
@@ -36,56 +36,8 @@ int findPlatform(int arr[], int dep[], int n)
 #include<bits/stdc++.h>
 using namespace std;
 
-#define          FaRaBi                 ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define          ll                     long long int
-#define          ld                     long double
-#define          pii                    pair < int, int>
-#define          pll                    pair < ll, ll>
-#define          MOD                    1000000007
-#define          ff                     first
-#define          ss                     second
-#define          pb                     push_back
-#define          pf                     printf
-#define          mp                     make_pair
-#define          gcd(a, b)              __gcd(a,b)
-#define          lcm(a, b)              ((a)*(b)/gcd(a,b))
-#define          PI                     acos(-1.0)
-#define          zero(a)                memset(a,0,sizeof a)
-#define          all(v)                 v.begin(),v.end()
-#define          Max(v)                 *max_element(all(v))
-#define          Min(v)                 *min_element(all(v))
-#define          Upper(c,x)             (upper_bound(c.begin(),c.end(),x)-c.begin())
-#define          Lower(c,x)             (lower_bound(c.begin(),c.end(),x)-c.begin())
-#define          Unique(X)              (X).erase(unique(all(X)),(X).end())
-#define          no                     cout << "NO" << endl ;
-#define          yes                    cout << "YES" << endl ;
-#define          segment_tree           int Lnode = node << 1 , Rnode = Lnode + 1 , mid = ( b + e ) >> 1 ;
-
-///--------------------**********----------------------------------
-
-vector < ll > v, v1, v2, v3, v4 ;
-vector < pair < ll, pll > > vec1, vec2 ;
-vector < pll > vec, Vec ;
-vector < ll > adj[ 100005 ] ;
-map < ll, ll > Mp, Mp1, Mp2 ;
-set < ll > st, st1, st2 ;
-queue < ll > Q ;
-stack < char > Stk ;
-multiset < ll > S ;
-vector < pair < string, int > > vs ;
-
-
-///---------------------**********--------------------------------
-
 ll n, sum ;
 string s ;
-
-
-
-void Reset()
-{
-
-}
 
 void Input()
 {
@@ -206,22 +158,6 @@ void Calculation()
     cout << Fun( str ) << endl ;
 }
 
-void solve()
-{
-    Reset() ;
-    Input() ;
-    Calculation() ;
-}
-
-int main()
-{
-    FaRaBi ;
-    int t ;
-    //cin >> t ;
-    t = 1 ;
-    while( t-- ) solve() ;
-    return 0 ;
-}
 
 ///  [[|,[&,1,[!,0]],[!,[|,[|,1,0],[!,1]]]]
 
